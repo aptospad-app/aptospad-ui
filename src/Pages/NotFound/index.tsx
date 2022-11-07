b37098d49f130fef40fc3399a8637f911097ddb2
Xuân Việt <viet27th@gmail.com>
2022-11-07 15:53:35 +0700

    Remove component

--- src/Pages/NotFound/index.tsx
+++ src/Pages/NotFound/index.tsx
@@ -1,26 +1,23 @@
 import React from "react";
 import "./index.scss";
 import {Link} from "react-router-dom";
-import BackgroundUniverse from "@/Components/BackgroundUniverse";
 
 export default function NotFoundScreen() {
   return (
     <div id="not-found">
-      <BackgroundUniverse>
-        <div className="wrap-page container-fluid d-flex flex-column justify-content-center align-items-center">
-          <div className="title">
+      <div className="wrap-page container-fluid d-flex flex-column justify-content-center align-items-center">
+        <div className="title">
             4
-            <img className="logo-icon" src="/images/logo_icon_gold.svg" alt="Buy crypto with paypal" />
+          <img className="logo-icon" src="/images/logo_icon_gold.svg" alt="Buy crypto with paypal" />
             4
-          </div>
-          <p className="description text-center">
-            <span>Look like you are</span>
-            <br/>
-            <span className="fw-light">lost in space</span>
-          </p>
-          <Link to="/" className="cbtn cbtn-outline-gradient-blue cbtn-lg text-uppercase">Go back home</Link>
         </div>
-      </BackgroundUniverse>
+        <p className="description text-center">
+          <span>Look like you are</span>
+          <br/>
+          <span className="fw-light">lost in space</span>
+        </p>
+        <Link to="/" className="cbtn cbtn-outline-gradient-blue cbtn-lg text-uppercase">Go back home</Link>
+      </div>
     </div>
   );
 }
